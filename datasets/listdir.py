ee5bfe41acf9f1b2888da22c9613a9096e99522e
YiWei0921 <2360696713@qq.com>
2024-05-22 17:25:00 +0800

    first

---
+++ datasets/listdir.py
@@ -0,0 +1,11 @@
+import os
+
+# folder path
+dir_path = r'D:\Document\BUAA\Knowledge\BS\yolov7-main\yolov7-main\datasets\grid\images\train'
+
+# list to store files
+
+
+# Iterate directory
+for path in os.listdir(dir_path):
+    print(r'/home/user/yolov7/datasets/grid/images/train/' + path)
